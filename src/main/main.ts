03293d90d9d6ad52c42265a4c7e26e88e7b915bd
junyoungkim <juny3738@gmail.com>
2023-05-13 14:10:29 +0900

    header menus

--- src/main/main.ts
+++ src/main/main.ts
@@ -26,9 +26,5 @@ function main() {
     }
   });
 }
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
+
 main();
--- src/renderer/components/header.tsx
+++ src/renderer/components/header.tsx
@@ -2,10 +2,70 @@ import React from 'react';
 
 // eslint-disable-next-line import/prefer-default-export, react/prefer-stateless-function
 export class Header extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor,@typescript-eslint/ban-types
+
   public render() {
+    const menuOpen = false;
     return (
       <>
         <header>
+          <div className="flex flex-wrap py-2">
+            <div className="w-full px-4">
+              <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 rounded">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                  <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
+                    <a
+                      className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                      href="#pablo"
+                    >
+                      pink Starter Menu
+                    </a>
+                    <button
+                      className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                      type="button"
+                      // onClick={() => setState({menuOpen : !state.menuOpen})}
+                    >
+                      <i className="fas fa-bars"></i>
+                    </button>
+                  </div>
+                  <div
+                    className={
+                      "lg:flex flex-grow items-center" +
+                      (menuOpen ? " flex" : " hidden")
+                    }
+                    id="example-navbar-info"
+                  >
+                    <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                      <li className="nav-item">
+                        <a
+                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                          href="#pablo"
+                        >
+                          Discover
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                          href="#pablo"
+                        >
+                          Profile
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                          href="#pablo"
+                        >
+                          Settings
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
         </header>
       </>
     );
